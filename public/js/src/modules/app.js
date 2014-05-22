923c55cc830a8ae432f74a520fcc20dfef47731e
Jesse Eikema <jesse@grr.nl>
2014-05-22 12:47:33 +0200

    code restructure functions

--- public/js/src/modules/app.js
+++ public/js/src/modules/app.js
@@ -14,17 +14,18 @@ if(!(req.url === "/favicon.ico")) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
     //send consumer_key to bitbucket to get the access token
 
-    authenticate();
-    function authenticate() {
-
-        var oauth = OAuth({
+    credentials();
+    function credentials() {
+    	var oauth = OAuth({
             consumer: {
                 public: 'CyXKdByytLCcNWRrXh',
                 secret: 'BtEUGzy8PQDnQAE5gcu3hGn4hBg56aSE'
             },
             signature_method: 'HMAC-SHA1'
         });
-
+    	authenticate(oauth);
+    }
+    function authenticate(oauth) {
         var request_data = {
             url: 'https://bitbucket.org/api/1.0/oauth/request_token',
             method: 'POST',
@@ -32,22 +33,24 @@ if(!(req.url === "/favicon.ico")) {
                 oauth_callback: 'http://loc.datavisualisatie.nl:1337'
             }
         };
-
         request({
             url: request_data.url,
             method: request_data.method,
             form: oauth.authorize(request_data)
         }, function(error, response, body) {
             // string manipulation to get the right value
-            var fetchString = JSON.stringify(response.body),
-            stringManipulation = fetchString.substring(53),
-            token = stringManipulation.slice(0, -31);
-            //redirect url to authenticate user
-            redirect(token, oauth);
+            var fetchString = JSON.stringify(response.body);
+
+            stringManipulation(fetchString, oauth);
             //console.log(oauth);
         });
     }
-
+    function stringManipulation(fetchString, oauth){
+    	var string = fetchString.substring(53),
+        token = string.slice(0, -31);
+        //redirect url to authenticate use
+        redirect(token, oauth);
+    }
     function redirect (token, oauth) {
     	//create redirect url
         var redirect = {
@@ -57,20 +60,15 @@ if(!(req.url === "/favicon.ico")) {
         request({
             url: redirect.url,
             method: redirect.method
-            }, function(error, response, body) {
-            	//start the retrieve function to fetch the send param from bitbucket
-    	        console.log(redirect.url);
-    	        retrieveToken(token, oauth);
+        }, function(error, response, body) {
+        	//start the retrieve function to fetch the send param from bitbucket
+	        console.log(redirect.url);
+	        retrieveToken(token, oauth);
         });
     }
-
 	function retrieveToken(token, oauth) {
-		var verifier = req.url,
-            createString = verifier.substring(17),
-            verifierToken = createString.slice(0, -31);
-
-        var Token = token,
-         	accessToken = Token.substring(12);
+		verifierToken();
+		accessToken(token);
 
         var new_request_data = {
             url: 'https://bitbucket.org/!api/1.0/oauth/access_token?',
@@ -82,21 +80,27 @@ if(!(req.url === "/favicon.ico")) {
         };
 
         var data = oauth.authorize(new_request_data);
-        console.log(data.oauth_token);
-     	//request token bug invalid request token
+
+     	//the auth_token is different and may be the cause of the problem
 
         request({
             url: new_request_data.url + 'oauth_version=' + data.oauth_version + '&oauth_nonce=' + data.oauth_nonce + '&oauth_timestamp=' + data.oauth_timestamp +
             								'&oauth_consumer_key=' + data.oauth_consumer_key + '&oauth_verifier=' + data.oauth_verifier +'&oauth_token=' +
-            									data.oauth_token + '&oauth_signature_method=' + data.oauth_signature_method + '&oauth_signature=' + data.oauth_signature,
+            									accessToken + '&oauth_signature_method=' + data.oauth_signature_method + '&oauth_signature=' + data.oauth_signature,
             method: new_request_data.method
         }, function(error, response, body) {
-			//console.log(response.client);
-			//console.log(data);
 			console.log(body);
         });
+    }
+    function verifierToken(){
+    	var verifier = req.url,
+            createString = verifier.substring(17),
+            verifierToken = createString.slice(0, -31);
+    }
+    function accessToken(token){
+    	var Token = token,
+         	accessToken = Token.substring(12);
     }
 }
 res.end();
 }).listen(1337,'loc.datavisualisatie.nl');
-// console.log('Server running at loc.datavisualisatie.nl');
