e92c9e4e6f85d78974dedd193179a81bec7a9e41
Jesse Eikema <jesse@grr.nl>
2014-06-25 18:01:41 +0200

    not realtime but working

--- public/express/public/javascripts/main.js
+++ public/express/public/javascripts/main.js
@@ -1,25 +1,23 @@
-var added = additions;
-var removed = removals;
-
-var files = filenames;
-var x = d3.scale.linear()
-    .domain([ d3.min(removed), d3.max(added)])
-    .range([ 0, 500]);
+var y = d3.scale.linear()
+    .domain([ 0, d3.max(allValues)])
+    .range([ 0, 250]);
 
 d3.select(".removed")
   .selectAll("div")
-    .data(added)
+    .data(additions)
   .enter().append("div")
-    .style("width", function(d) {  return x(d) + "px"; });
-    //.text(function(d) { return d; });
+    .style("width", function(d) {  return y(d) + "px"; });
+    // .text(function(d) { return d; });
 
 d3.select(".added")
   .selectAll("div")
-    .data(removed)
+    .data(removals)
   .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; });
-    //.text(function(d) { return d; });
+    .style("width", function(d) { return y(d) + "px"; });
+    // .text(function(d) { return d; });
 
-
-console.log('added ' + added);
-console.log('removed ' + removed);
+// console.log('comments' + comments);
+// console.log('authors' + authors);
+console.log('added ' + additions);
+console.log('removed ' + removals);
+console.log('all' + allValues);
--- public/express/routes/index.js
+++ public/express/routes/index.js
@@ -1,50 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
-var bitbucket = require('bitbucket-rest');
 var d3 = require('d3');
+
 var additions = [];
 var removals = [];
 var filenames = [];
 var author = [];
 var comment = [];
-var accountname = "grrr",
-  repo = "melkweg";
-//   node = '14aebcaec6da905ae09abd0e3a7b4d2e2ebca531';
-
-var client = bitbucket.connectClient({username : 'jesseeikema', password : 'Eikema22'});
-
-//only works once when server is started
-//fix : how to do it multiple times
 var nodeBucket = [];
-//get a list of commits
-
+var allValues = [];
+var accountname = "grrr",
+    repo = "melkweg";
 var owner = "grrr",
     repo_slug = repo;
 
-dataGetter(owner, repo_slug);
+dataGetter();
 
-function dataGetter(owner, repo_slug){
+function dataGetter(){
   unirest.get('https://bitbucket.org/api/2.0/repositories/'+owner+'/'+repo_slug+'/commits').auth({
     user: 'jesseeikema',
     pass: 'Eikema22',
     sendImmediately: true
   }).end(function(response){
     var data = response.body.values;
-    data.length =  15;
-    for (var i = 0; i < data.length; i++) {
+    data.length =  20;
+    for (var i = 0, len = data.length; i < len; i++) {
       data[i];
       author.push(data[i].author.user.display_name);
-      comment.push(data[i].message);
+      //comment.push(data[i].message);
+      //if has already exists remove it
       nodeBucket.push(data[i].hash);
-    };
+    }
     dataSetter();
   });
-}
+};
+
+// if hash already exists remove the old one
 
 function dataSetter(){
   console.log(nodeBucket);
-  nodeBucket.length = 15;
+  console.log(author, comment);
+  nodeBucket.length = 20;
   for (var i = 0; i < nodeBucket.length; i++) {
     nodeBucket[i];
     unirest.get('https://bitbucket.org/api/1.0/repositories/'+accountname+'/'+repo+'/changesets/'+nodeBucket[i]+'/diffstat').auth({
@@ -53,24 +50,26 @@ function dataSetter(){
     sendImmediately: true
   }).end(function(response){
     var dataSet = response.body;
-    for (var i = 0; i < dataSet.length; i++) {
+    for (var i = 0, len = dataSet.length; i < len; i++) {
       dataSet[i];
-      // console.log(dataSet[i]);
-      // filenames.push(dataSet[i].file);
       additions.push(dataSet[i].diffstat.added);
       removals.push(dataSet[i].diffstat.removed);
+      allValues.push(additions.concat(removals));
     };
-    // console.log(additions);
-    // console.log(removals);
   });
-
   };
-
 };
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'additions and removals per commit', additions: d3.values(additions), removals: d3.values(removals) } );
+  res.render('index', {
+      // init: dataGetter(),
+      authors: author,
+      comments: comment,
+      additions: additions,
+      removals: removals,
+      allValues: allValues
+  })
 });
 
 module.exports = router;
